af343593f527b9a47d8e8a42782b65b240f3de14
Sonalika Sahoo <sonalika2405@gmail.com>
2021-06-30 19:34:38 +0530

    fixed bugs, sign in functiionality working well

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:an_agile_squad/backend/firebase_repository.dart';
 import 'package:an_agile_squad/screens/home_screen.dart';
 import 'package:an_agile_squad/screens/login_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
