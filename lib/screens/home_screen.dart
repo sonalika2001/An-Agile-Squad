42dd469f81e508e595c7b852e973514caa636615
Sonalika Sahoo <sonalika2405@gmail.com>
2021-07-04 19:20:52 +0530

    added bottom navigation bar ,slide functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,103 @@
+import 'package:an_agile_squad/utils/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController;
+  int _page = 0;
+  double _labelFontSize = 10;
+
+  @override
+  void initState() {
+    
+    super.initState();
+    pageController = PageController();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page){
+    pageController.jumpToPage(page);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      backgroundColor: kblackColor,
+      body: PageView(
+        children: <Widget>[
+          Center(
+            child: Text('Chat List Screen'),
+          ),
+          Center(
+            child: Text('Call Logs'),
+          ),
+          Center(
+            child: Text('Contact Screen'),
+          )
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: kblackColor,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.chat,
+                  color: (_page == 0) ? klightBlueColor : kgreyColor,
+                ),
+                title: Text(
+                  'Chats',
+                  style: TextStyle(
+                    fontSize:_labelFontSize,
+                    color: (_page == 0) ? klightBlueColor : kgreyColor,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.call,
+                  color: (_page == 1) ? klightBlueColor : kgreyColor,
+                ),
+                title: Text(
+                  'Call Log',
+                  style: TextStyle(
+                    fontSize:_labelFontSize,
+                    color: (_page == 1) ? klightBlueColor : kgreyColor,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.contact_phone,
+                  color: (_page == 2) ? klightBlueColor : kgreyColor,
+                ),
+                title: Text(
+                  'Contact',
+                  style: TextStyle(
+                    fontSize:_labelFontSize,
+                    color: (_page == 2) ? klightBlueColor : kgreyColor,
+                  ),
+                ),
+              ),
+            ],
+            onTap: navigationTapped,
+            currentIndex: _page,
+          ),
+        ),
       ),
     );
   }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//constant variables in flutter are conventianally named with a 'k' as the prefix
+const Color kblueColor = Color(0xff2b9ed4);
+const Color kblackColor = Color(0xff19191b);
+const Color kgreyColor = Color(0xff8f8f8f);
+const Color kuserCircleBackground = Color(0xff2b2b33);
+const Color konlineDotColor = Color(0xff46dc64);
+const Color klightBlueColor = Color(0xff0077d7);
+const Color kseparatorColor = Color(0xff272c35);
+
+const Color kgradientColorStart = Color(0xff00b6f3);
+const Color kgradientColorEnd = Color(0xff0184dc);
+
+const Color ksenderColor = Color(0xff2b343b);
+const Color kreceiverColor = Color(0xff1e2225);
+
+Gradient kfabGradient = LinearGradient(
+      colors: [kgradientColorStart, kgradientColorEnd],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight);
+
+
